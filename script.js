dfaced74c7a9859213b44b77ed8aa24f1d11479a
teod23 <168969458+teod23@users.noreply.github.com>
2024-08-25 12:53:36 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const array = [10, 20, 30, 40, 50];
+const valueToFind = 30;
+console.log(array.includes(valueToFind));
+
+if (array.includes(valueToFind)) {
+    console.log('რიცხვი ნაპოვნია');
+} else {
+    console.log('რიცხვი არ მოიძებნა');
+}
